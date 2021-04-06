1351afa9be8fa2fe5092cf3cfc0fff8cffea607a
ganweilun <ganweilun@hikvision.com.cn>
2021-04-06 20:24:28 +0800

    update:chess

--- src/components/chess.js
+++ src/components/chess.js
@@ -1,28 +1,48 @@
-import React from 'react'
+import React from 'react';
 
 class Square extends React.Component {
+    // constructor(props){
+    //     super(props)
+    //     this.state={
+    //         value:null
+    //     }
+    // }
+    /* 改进 将state放到父组件里进行判断 */
     render() {
         return (
             <button
                 className="square"
                 onClick={() => {
-                    alert(this.props.value)
-                }}
-            >
+                    this.props.onClick(this.props.value);
+                }}>
                 {this.props.value}
             </button>
-        )
+        );
     }
 }
 
 class Board extends React.Component {
-    renderSquare(i) {
-        return <Square value={i} />
+    constructor() {
+        super();
+        this.state = {
+            squareArr: Array(9).fill(null),
+            nextFlag: true
+        };
     }
-
+    renderSquare(i) {
+        return (
+            <Square
+                value={this.state.squareArr[i]}
+                onClick={(params) => {
+                    this.handleClick(i, params);
+                }}
+            />
+        );
+    }
+// state改变触发render函数  故判断胜者在此函数中
     render() {
-        const status = 'Next player: X'
-
+        let winner=calculateWinner(this.state.squareArr)
+        const status= winner?`The winner is ${winner}`:`Next player: ${this.state.nextFlag ? 'X' : 'O'}`;
         return (
             <div>
                 <div>这是第{this.props.t}个棋盘</div>
@@ -43,7 +63,16 @@ class Board extends React.Component {
                     {this.renderSquare(8)}
                 </div>
             </div>
-        )
+        );
+    }
+    handleClick(i, params) {
+        if (this.state.squareArr[i]||calculateWinner(this.state.squareArr)) {
+            return;
+        }
+        let resultArr = [...this.state.squareArr];
+        resultArr[i] = this.state.nextFlag ? 'X' : 'O';
+        this.setState({ squareArr: resultArr, nextFlag: !this.state.nextFlag });
+        console.log(params);
     }
 }
 
@@ -59,7 +88,26 @@ class Game extends React.Component {
                     <ol>{/* TODO */}</ol>
                 </div>
             </div>
-        )
+        );
     }
 }
-export default Game
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
+export default Game;
